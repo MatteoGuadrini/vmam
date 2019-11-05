8b1d71884f73fab8c798823987e92fa9b5a0fa77
Matteo Guadrini <matteo.guadrini@hotmail.it>
2019-11-05 16:46:41 +0100

    Add docstring for action synopsys

--- vmam.py
+++ vmam.py
@@ -19,3 +19,29 @@
 #
 #     You should have received a copy of the GNU General Public License
 #     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+Welcome to doc of vmam: VLAN Mac-address Authentication Manager
+
+    SYNOPSYS
+
+        vmam [action] [parameter] [options]
+
+    USAGE
+
+        config {action}: Configuration command for vmam environment
+
+            --new/-n {parameter}: Instruction to create a new configuration file. By specifying a path, it creates the
+            file in the indicated path. The default path is /etc/vmam/vmam.cfg
+
+            $> vmam config --new
+            Create a new configuration in a standard path: /etc/vmam/vmam.cfg
+
+            --get-cmd/-g {parameter}: Instruction to obtain the appropriate commands to configure your network
+            infrastructure and radius server around the created configuration file. By specifying a path, it creates
+            the file in the indicated path. The default path is /etc/vmam/vmam.cfg
+
+            $> vmam config --get-cmd
+            It takes instructions to configure its own network and radius server structure,
+            from standard path: /etc/vmam/vmam.cfg
+"""
