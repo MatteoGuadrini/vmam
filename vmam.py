21ef635694949a2c0e7d1e8c08bd31e43a6f7f98
Matteo Guadrini <matteo.guadrini@hotmail.it>
2020-08-28 10:13:48 +0200

    Added support for FreeIpa remove user

--- vmam.py
+++ vmam.py
@@ -1433,8 +1433,7 @@ if __name__ == '__main__':
             mac, ','.join(config['LDAP']['servers'])))
         login_attr = 'samaccountname' if ldap_v == 'MS-LDAP' else 'uid'
         u_search_attributes = ['samaccountname', 'description'] if ldap_v == 'MS-LDAP' else ['uid', 'description']
-        ret = query_ldap(bind, config['LDAP']['mac_user_base_dn'], u_search_attributes,
-                         **{login_attr: mac})
+        ret = query_ldap(bind, config['LDAP']['mac_user_base_dn'], u_search_attributes, **{login_attr: mac})
         if ret and ret[0].get('dn'):
             force = confirm('Do you want to disable {0} mac-address?'.format(mac)) if not arguments.force else True
             if force:
@@ -1478,7 +1477,9 @@ if __name__ == '__main__':
         # Query: check if mac-address exist
         debugger(arguments.verbose, logger, 'Exist mac-address {0} on LDAP servers {1}?'.format(
             mac, ','.join(config['LDAP']['servers'])))
-        ret = query_ldap(bind, config['LDAP']['mac_user_base_dn'], ['samaccountname'], samaccountname=mac)
+        login_attr = 'samaccountname' if ldap_v == 'MS-LDAP' else 'uid'
+        u_search_attributes = ['samaccountname'] if ldap_v == 'MS-LDAP' else ['uid']
+        ret = query_ldap(bind, config['LDAP']['mac_user_base_dn'], u_search_attributes, **{login_attr: mac})
         if ret and ret[0].get('dn'):
             force = confirm('Do you want to delete {0} mac-address?'.format(mac)) if not arguments.force else True
             if force:
